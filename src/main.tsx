b18b83ec9052cdc012a3535be10fd653d978225c
Santy <pizarro.santy@gmail.com>
2026-06-13 12:56:25 -0300

    Use compatible GLB texture loader

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,14 @@ import ReactDOM from "react-dom/client";
 import { BestiaryScreen } from "./screens/BestiaryScreen";
 import "./styles.css";
 
+// TextureLoader is more reliable for PNGs embedded in GLBs under restrictive CSP headers.
+if (typeof window.createImageBitmap === "function") {
+  Object.defineProperty(window, "createImageBitmap", {
+    configurable: true,
+    value: undefined,
+  });
+}
+
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <BestiaryScreen />
